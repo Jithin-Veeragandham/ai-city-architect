2b87e627b06f5be15f4d6d2305e5fa7cd47b2e63
SahilBane2002 <sahil.rosefield@gmail.com>
2024-11-26 23:06:07 -0500

    Included Genetic Algorithm

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
 from local_search import *
 from grid_generation import *
+from genetic_algo import *
+from visualization import *
 
 
 def main():
     initial_grid = generate_city_grid_with_only_bordering_intersections()
-    initial_grid_with_intersections  = place_intersections_in_every_column_randomly(initial_grid)
-    best_grid, shortest_paths = hill_climbing(initial_grid_with_intersections, max_iterations=1000)
+    initial_grid_with_intersections = place_intersections_in_every_column_randomly(
+        initial_grid)
+    best_grid, shortest_paths = hill_climbing(
+        initial_grid_with_intersections, max_iterations=1000)
+
+    optimized_grid = genetic_algorithm(path_penalty=1, intersection_penalty=1000,
+                                       population_size=10, generations=10, mutation_rate=0.1, initial_grid=initial_grid)
+    short_paths = find_all_shortest_paths(optimized_grid)
+    dir_path = os.path.join(
+        rf"D:\NEU MS CS\FAI\Project\ai-city-architect\res\GeneticAlgorithm")
+
+    save_city_grid(optimized_grid, short_paths, dir_path,
+                   f"Final.png")
 
 
 if __name__ == "__main__":
--- src/visualization.py
+++ src/visualization.py
@@ -16,10 +16,10 @@ def load_images(cell_size):
     """
     # print("Loading images...")
     images = {
-        1: Image.open(rf"/Users/shreycshah/Desktop/Coursework/Fall24/CS5100/Project/UrbanAItect/res//building.jpg").resize(
+        1: Image.open(rf"D:\NEU MS CS\FAI\Project\ai-city-architect\res\building.jpg").resize(
             (cell_size, cell_size)
         ),  # Building image (JPEG format)
-        2: Image.open(rf"/Users/shreycshah/Desktop/Coursework/Fall24/CS5100/Project/UrbanAItect/res//emergency.png").resize(
+        2: Image.open(rf"D:\NEU MS CS\FAI\Project\ai-city-architect\res\emergency.png").resize(
             (cell_size, cell_size)
         ),  # Emergency service image (PNG format)
     }
@@ -58,14 +58,14 @@ def save_city_grid(grid, paths, dir_name, output_file_name):
     intersection_color = (52, 251, 152)  # Green
     road_color = (169, 169, 169)  # Gray
     road_border_color = (255, 255, 255)  # White
-    border_thickness =  6 # Thickness of the road border
-    
+    border_thickness = 6  # Thickness of the road border
+
     # Path colors
-    path_colors = [(255, 0, 0), (0, 0, 255), (255, 165, 0), (255, 255, 0), 
-                   (0, 255, 255), (255, 0, 255), (128, 0, 128), (0, 128, 128), 
-                   (255, 192, 203), (0, 100, 0), (139, 69, 19), (75, 0, 130), 
+    path_colors = [(255, 0, 0), (0, 0, 255), (255, 165, 0), (255, 255, 0),
+                   (0, 255, 255), (255, 0, 255), (128, 0, 128), (0, 128, 128),
+                   (255, 192, 203), (0, 100, 0), (139, 69, 19), (75, 0, 130),
                    (128, 128, 128), (100, 149, 237), (199, 21, 133)]
-    
+
     path_thickness = 6  # Border thickness for paths
     max_offset = 6  # Maximum offset to avoid overlapping
 
@@ -82,16 +82,21 @@ def save_city_grid(grid, paths, dir_name, output_file_name):
         for x in range(grid_size):
             top_left = (x * CELL_SIZE, y * CELL_SIZE)
             bottom_right = (top_left[0] + CELL_SIZE, top_left[1] + CELL_SIZE)
-            
+
             if grid[y][x] == 3:  # Intersection
-                draw.rectangle([top_left, bottom_right], fill=intersection_color)
+                draw.rectangle([top_left, bottom_right],
+                               fill=intersection_color)
             elif grid[y][x] == 0:  # Road
                 # Draw road border
-                draw.rectangle([top_left, bottom_right], fill=road_border_color)
+                draw.rectangle([top_left, bottom_right],
+                               fill=road_border_color)
                 # Draw inner road
-                inner_top_left = (top_left[0] + border_thickness, top_left[1] + border_thickness)
-                inner_bottom_right = (bottom_right[0] - border_thickness, bottom_right[1] - border_thickness)
-                draw.rectangle([inner_top_left, inner_bottom_right], fill=road_color)
+                inner_top_left = (
+                    top_left[0] + border_thickness, top_left[1] + border_thickness)
+                inner_bottom_right = (
+                    bottom_right[0] - border_thickness, bottom_right[1] - border_thickness)
+                draw.rectangle(
+                    [inner_top_left, inner_bottom_right], fill=road_color)
             elif grid[y][x] in images:  # Buildings or emergency services
                 # Paste the corresponding image
                 img.paste(images[grid[y][x]], top_left)
@@ -106,12 +111,13 @@ def save_city_grid(grid, paths, dir_name, output_file_name):
             offset_y = (i % max_offset) * 2  # Offset in y-direction
 
             # Define the rectangle's corners for the hollow square
-            path_top_left = (x * CELL_SIZE + offset_x, y * CELL_SIZE + offset_y)
+            path_top_left = (x * CELL_SIZE + offset_x,
+                             y * CELL_SIZE + offset_y)
             path_bottom_right = (
                 path_top_left[0] + CELL_SIZE - max_offset,
                 path_top_left[1] + CELL_SIZE - max_offset,
             )
-            
+
             # Draw a hollow square (thick border only) for the path
             draw.rectangle(
                 [path_top_left, path_bottom_right],
